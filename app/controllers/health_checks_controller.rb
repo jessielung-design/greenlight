556d78dba6bd0a5f62049d2aee242f69a571569d
bsolcndev01 <cndev246@gmail.com>
2026-09-22 16:58:44 +0800

    Fix database and BBB health probes

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -38,7 +38,7 @@ class HealthChecksController < ApplicationController
   private
 
   def check_database
-    raise 'Unable to connect to Database' unless ActiveRecord::Base.connection.active?
+    ActiveRecord::Base.connection_pool.with_connection { |connection| connection.select_value('SELECT 1') }
     raise 'Unable to connect to Database - pending migrations' unless ActiveRecord::Migration.check_all_pending!.nil?
   rescue StandardError => e
     raise "Unable to connect to Database - #{e}"
@@ -69,8 +69,9 @@ class HealthChecksController < ApplicationController
   end
 
   def check_big_blue_button
-    checksum = Digest::SHA1.hexdigest("isMeetingRunningmeetingID=0#{Rails.configuration.bigbluebutton_secret}")
-    uri = URI("#{Rails.configuration.bigbluebutton_endpoint}isMeetingRunning?meetingID=0&checksum=#{checksum}")
+    meeting_id = 'greenlight-health-check'
+    checksum = Digest::SHA1.hexdigest("isMeetingRunningmeetingID=#{meeting_id}#{Rails.configuration.bigbluebutton_secret}")
+    uri = URI("#{Rails.configuration.bigbluebutton_endpoint}isMeetingRunning?meetingID=#{meeting_id}&checksum=#{checksum}")
     res = Net::HTTP.get(uri)
     doc = Nokogiri::XML(res)
 
--- spec/controllers/health_checks_controller_spec.rb
+++ spec/controllers/health_checks_controller_spec.rb
@@ -42,14 +42,14 @@ RSpec.describe HealthChecksController, type: :controller do
       end
 
       it 'returns success' do
-        allow(ActiveRecord::Base).to receive(:connected?).and_return(true)
+        expect(ActiveRecord::Base.connection).to receive(:select_value).with('SELECT 1').and_call_original
         get :check
         expect(response.body).to eq('success')
         expect(response).to have_http_status(:ok)
       end
 
       it 'returns failure message' do
-        allow(ActiveRecord::Base.connection).to receive(:active?).and_return(false)
+        allow(ActiveRecord::Base.connection).to receive(:select_value).with('SELECT 1').and_raise(ActiveRecord::ConnectionNotEstablished)
         get :check
         expect(response.body).to include('Unable to connect to Database')
         expect(response).to have_http_status(:internal_server_error)
